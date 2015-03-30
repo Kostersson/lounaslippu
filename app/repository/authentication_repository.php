2e2a6ea38338549828af78ff0515d8ff19b77340
Pauli Kostamo <pauli.kostamo@w3.fi>
2015-03-30 23:46:11 +0300

    debug

--- app/repository/authentication_repository.php
+++ app/repository/authentication_repository.php
@@ -8,6 +8,7 @@ class AuthenticationRepository{
 
         $query = DB::connection()->prepare('SELECT FROM `users` WHERE email = ":email" AND password = ":password"');
         $query->execute(array('email' => $username, 'password' => $password));
+        die(var_dump(DB::connection(), $query->fetchAll()));
         return $query->fetchAll();
     }
 }
