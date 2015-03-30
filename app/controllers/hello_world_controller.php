ac60c25e332c7c2e140654857a602a131dcf2e59
Pauli Kostamo <pauli.kostamo@w3.fi>
2015-03-30 14:53:50 +0300

    login view

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,15 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+class HelloWorldController extends BaseController{
 
     public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+        check_logged_in();
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+        View::make('home.html');
     }
 
     public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
+        // Testaa koodiasi täällä
+        echo 'Hello World!';
     }
-  }
+}
--- lib/base_controller.php
+++ lib/base_controller.php
@@ -8,8 +8,7 @@
     }
 
     public static function check_logged_in(){
-      // Toteuta kirjautumisen tarkistus tähän.
-      // Jos käyttäjä ei ole kirjautunut sisään, ohjaa hänet toiselle sivulle (esim. kirjautumissivulle).
+      Redirect::to("/sisaankirjautuminen");
     }
 
   }
